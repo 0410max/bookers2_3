38c21a548ad13a82aa0cb3f2e6d163d35e85c750
Taiga Hashimoto <2002421taiga@gmail.com>
2023-02-16 14:10:16 +0900

    2.16.14

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @user = current_user
+    @books = Book.all
+  end
+
+  def show
+    @new_book = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = 'You have created book successfully.'
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = 'You have deleted book successfully.'
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:notice] = 'You have updated book successfully.'
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def book_params 
+    params.require(:book).permit(:title,:body)
+  end
+end
+
